db271d486814e406284db98862187305befb5b54
Ibrahim Soliman <ibrahimsoliman.mpputem1617@gmail.com>
2019-09-08 22:12:16 +0800

    Update AnnotationTool.py

--- AnnotationTool.py
+++ AnnotationTool.py
@@ -12,17 +12,17 @@ import os.path
 
 class Annot:
 
-    def __init__(self, rect, text=''):
+    def __init__(self, rect, classIndex=0):
         if rect is None:
             rect = ((0, 0), (0, 0), 0)
         self.rect = rect
-        self.text = text
+        self.classIndex = classIndex
 
     def __repr__(self):
-        return 'Annotation {} "{}"'.format(self.rect, self.text)
+        return 'Annotation {} "{}"'.format(self.rect, self.classIndex)
 
 
-def parse_annotation_file(path):
+def parse_annotation_file(path, h, w):
     lines = None
     with open(path) as f:
         lines = [l.strip().split() for l in f.readlines()]
@@ -33,36 +33,36 @@ def parse_annotation_file(path):
         if len(l) < 5:
             raise Exception('Failed to parse line "{}"'.format(l))
 
-        rect = [float(s) for s in l[:5]]
-        text = '' if len(l) == 5 else ' '.join(l[5:])
-        ret.append(Annot((tuple(rect[0:2]), tuple(rect[2:4]), rect[4]), text))
+        rect = [float(s) for s in l[1:5]]
+        angle = 0.0 if len(l) == 5 else float(l[5])
+        ret.append(Annot(((rect[0]*w,rect[1]*h), (rect[2]*w,rect[3]*h), angle), l[0]))
     return ret
 
 
-def write_annotation_file(data, to):
+def write_annotation_file(data, to, h, w):
     with open(to, 'w') as f:
         for d in data:
-            f.write('{:.0f} {:.0f} {:.0f} {:.0f} {:.3f} {}\n'.format(d.rect[0][0], d.rect[0][1], d.rect[1][0], d.rect[1][1], d.rect[2], d.text))
+            f.write('{:d} {:.6f} {:.6f} {:.6f} {:.6f} {:.2f}\n'.format(int(d.classIndex), (d.rect[0][0]/w), (d.rect[0][1]/h), (d.rect[1][0]/w), (d.rect[1][1]/h), d.rect[2]))
 
-def write_annotation_file_90(data, to,h):
+def write_annotation_file_90(data, to, h, w):
     with open(to, 'w') as f:
         for d in data:
-            f.write('{:.0f} {:.0f} {:.0f} {:.0f} {:.3f} {}\n'.format((h-d.rect[0][1]), d.rect[0][0], d.rect[1][0],d.rect[1][1], (d.rect[2]+90.0), d.text))
+            f.write('{:d} {:.6f} {:.6f} {:.6f} {:.6f} {:.2f}\n'.format(int(d.classIndex),((h-d.rect[0][1])/w), (d.rect[0][0]/h), (d.rect[1][0]/w), (d.rect[1][1]/h), (d.rect[2]+90.0)))
 
-def write_annotation_file_180(data, to,h,w):
+def write_annotation_file_180(data, to, h, w):
     with open(to, 'w') as f:
         for d in data:
-            f.write('{:.0f} {:.0f} {:.0f} {:.0f} {:.3f} {}\n'.format((w-d.rect[0][0]), (h-d.rect[0][1]), d.rect[1][0], d.rect[1][1], d.rect[2], d.text))
+            f.write('{:d} {:.6f} {:.6f} {:.6f} {:.6f} {:.2f}\n'.format(int(d.classIndex), ((w-d.rect[0][0])/w), ((h-d.rect[0][1])/h), (d.rect[1][0]/w), (d.rect[1][1]/h), d.rect[2]))
 
-def write_annotation_file_270(data, to,w):
+def write_annotation_file_270(data, to, h, w):
     with open(to, 'w') as f:
         for d in data:
-            f.write('{:.0f} {:.0f} {:.0f} {:.0f} {:.3f} {}\n'.format((d.rect[0][1]), (w-d.rect[0][0]), d.rect[1][0], d.rect[1][1], (d.rect[2]+90.0), d.text))
+            f.write('{:d} {:.6f} {:.6f} {:.6f} {:.6f} {:.2f}\n'.format(int(d.classIndex), (d.rect[0][1]/w), ((w-d.rect[0][0])/h), (d.rect[1][0]/w), (d.rect[1][1]/h), (d.rect[2]+90.0)))
 
-def write_annotation_file_flip(data, to,w):
+def write_annotation_file_flip(data, to, h, w):
     with open(to, 'w') as f:
         for d in data:
-            f.write('{:.0f} {:.0f} {:.0f} {:.0f} {:.3f} {}\n'.format((w-d.rect[0][0]), d.rect[0][1], d.rect[1][0], d.rect[1][1], d.rect[2], d.text))
+            f.write('{:d} {:.6f} {:.6f} {:.6f} {:.6f} {:.2f}\n'.format(int(d.classIndex), ((w-d.rect[0][0])/w), (d.rect[0][1]/h), (d.rect[1][0]/w), (d.rect[1][1]/h), d.rect[2]))
 
 
 def mat_to_qimage(mat):
@@ -73,23 +73,39 @@ def mat_to_qimage(mat):
 def increase_brightness(img, value=30):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     h, s, v = cv2.split(hsv)
-
     lim = 255 - value
     v[v > lim] = 255
     v[v <= lim] += value
-
     final_hsv = cv2.merge((h, s, v))
     img = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
     return img
 
 class MouseLabel(QtGui.QLabel):
 
-    def __init(self, parent):
+    def __init__(self, parent):
         QtGui.QLabel.__init__(self, parent)
+        self.begin = QtCore.QPoint()
+        self.end = QtCore.QPoint()
 
-    def mouseReleaseEvent(self, ev):
-        self.emit(QtCore.SIGNAL('clicked(int, int, int)'), ev.x(), ev.y(), ev.button())
+    def updatePixmap(self, imagePixmap):
+	    self.imagePix = imagePixmap	
+  
+    def mousePressEvent(self, event):
+        self.begin = event.pos()
+        self.end = event.pos()
+        self.flag = False
+        self.update()
 
+    def mouseMoveEvent(self, event):
+        self.setPixmap(self.imagePix)
+        painter = QtGui.QPainter(self.pixmap())
+        self.end = event.pos()
+        painter.drawRect(QtCore.QRect(self.begin, self.end))
+        painter.end()
+        self.update()  
+    
+    def mouseReleaseEvent(self, event):
+        self.emit(QtCore.SIGNAL('clicked(int, int, int, int, int)'), self.begin.x(), self.begin.y(), self.end.x(), self.end.y(), event.button())
 
 class Window(QtGui.QWidget):
 
@@ -137,6 +153,12 @@ class Window(QtGui.QWidget):
         self.saveYoloFormat.setFixedSize(int(1.8*iconsize),int(1.4*iconsize))
         self.saveYoloFormat.setToolButtonStyle(QtCore.Qt.ToolButtonTextUnderIcon)
         self.saveYoloFormat.clicked.connect(self.saveAnnotation)
+        classlabel = QtGui.QLabel("Classes")
+        self.classList = [line.rstrip('\n') for line in open('./classes.txt')]
+	self.classIndx=0
+        self.CBclass = QtGui.QComboBox()
+        self.CBclass.addItems(self.classList)
+        self.CBclass.currentIndexChanged.connect(self.selectionchange)
        
         l1 = QtGui.QLabel()
         l1.setText("Data Augmentation")
@@ -177,39 +199,41 @@ class Window(QtGui.QWidget):
         self.addBright.setToolButtonStyle(QtCore.Qt.ToolButtonTextUnderIcon)
         self.addBright.clicked.connect(self.addBrightImage)
 
-	utemlogo = QtGui.QLabel()
+        utemlogo = QtGui.QLabel()
         pixmap = QtGui.QPixmap('./res/utem.png')
         utemlogo.setPixmap(pixmap.scaled(int(2*iconsize),int(1.5*iconsize),QtCore.Qt.KeepAspectRatio))
         fkekklogo = QtGui.QLabel()
         pixmap = QtGui.QPixmap('./res/fkekk.png')
         fkekklogo.setPixmap(pixmap.scaled(int(2*iconsize),int(1.5*iconsize),QtCore.Qt.KeepAspectRatio))
 
-	ButtonLayout = QtGui.QVBoxLayout()
-	ButtonLayout.setAlignment(QtCore.Qt.AlignCenter)
-	ButtonLayout.addWidget(self.buttonopenFolder)
-	hboxRL = QtGui.QHBoxLayout()
-	hboxRL.addWidget(self.nextImageButton)
-	hboxRL.addWidget(self.previousImageButton)
-	ButtonLayout.addLayout(hboxRL)
-	ButtonLayout.addWidget(self.saveYoloFormat)
-   	ButtonLayout.addStretch()
-	ButtonLayout.addWidget(l1)
-	ButtonLayout.addWidget(self.rotateImage)
-	ButtonLayout.addWidget( self.flipping)
-	ButtonLayout.addWidget(self.addnoise)
-	ButtonLayout.addWidget(self.addBright)
-   	ButtonLayout.addStretch()
-	ButtonLayout.addWidget(fkekklogo)
-	ButtonLayout.addWidget(utemlogo)
+        ButtonLayout = QtGui.QVBoxLayout()
+        ButtonLayout.setAlignment(QtCore.Qt.AlignCenter)
+        ButtonLayout.addWidget(self.buttonopenFolder)
+        hboxRL = QtGui.QHBoxLayout()
+        hboxRL.addWidget(self.nextImageButton)
+        hboxRL.addWidget(self.previousImageButton)
+        ButtonLayout.addLayout(hboxRL)
+        ButtonLayout.addWidget(self.saveYoloFormat)
+        ButtonLayout.addWidget(classlabel)
+        ButtonLayout.addWidget(self.CBclass)
+        ButtonLayout.addStretch()
+        ButtonLayout.addWidget(l1)
+        ButtonLayout.addWidget(self.rotateImage)
+        ButtonLayout.addWidget( self.flipping)
+        ButtonLayout.addWidget(self.addnoise)
+        ButtonLayout.addWidget(self.addBright)
+        ButtonLayout.addStretch()
+        ButtonLayout.addWidget(fkekklogo)
+        ButtonLayout.addWidget(utemlogo)
 
         self.label_image = MouseLabel(self)
         self.scroll = QtGui.QScrollArea(self)
         self.scroll.setWidget(self.label_image)
-	hbox = QtGui.QHBoxLayout(self)
-	hbox.addWidget(self.scroll)
-	hbox.addLayout(ButtonLayout)
+        hbox = QtGui.QHBoxLayout(self)
+        hbox.addWidget(self.scroll)
+        hbox.addLayout(ButtonLayout)
 
-        self.connect(self.label_image, QtCore.SIGNAL('clicked(int, int, int)'), self.point_selected)
+        self.connect(self.label_image, QtCore.SIGNAL('clicked(int, int, int, int, int)'), self.point_selected)
 
     def openFloder(self):
         dlg = QtGui.QFileDialog(self)
@@ -223,7 +247,7 @@ class Window(QtGui.QWidget):
             annot_data = []
             if os.path.isfile(annot_name):
                 print('Loading existing annotation file')
-                annot_data = parse_annotation_file(annot_name)
+                annot_data = parse_annotation_file(annot_name,img.shape[0],img.shape[1])
             self.newImage(filenames, img, annot_name, annot_data)
             self.redraw()
 
@@ -233,22 +257,18 @@ class Window(QtGui.QWidget):
         self.annot_path = annot_path
         self.annot = annot
         self.annot_coords = [[0, 0], [0, 0], [0, 0], [0, 0]]  # x;y
-        self.annot_idx = 0
+        self.label_image.updatePixmap(QtGui.QPixmap.fromImage(mat_to_qimage(self.image)))
 
-    def point_selected(self, x, y, button):
+    def point_selected(self, x1, y1, x2 ,y2, button):
         if button != 1 and button != 2:
             return
         if button == 1:
             # new annotation point
-            self.annot_coords[self.annot_idx][0] = x
-            self.annot_coords[self.annot_idx][1] = y
-            self.annot_idx += 1
-            if self.annot_idx == 4:
-                rect = cv2.minAreaRect(np.array(self.annot_coords))
-                self.annot.append(Annot(rect))
-                self.annot_idx = 0
-        if button == 2:
-            # remove existing annotation
+            w = float(x2-x1)
+            h = float(y2-y1)
+            rect = ((float(x1)+(w/2),float(y1)+(h/2)),(w,h),0.0)  
+            self.annot.append(Annot(rect, self.classIndx))   #Add ID of seleted Class
+        if button == 2:                     # remove existing annotation
             id = 0
             del_id = -1
             for an in self.annot:
@@ -256,7 +276,7 @@ class Window(QtGui.QWidget):
                 bp = cv2.boxPoints(an.rect)
                 bp = np.int0(bp)
                 cv2.drawContours(tmp, [bp], 0, (255), -1)
-                if tmp[y, x] > 0:
+                if tmp[y1, x1] > 0:
                     del_id = id
                     break
                 id += 1
@@ -270,15 +290,13 @@ class Window(QtGui.QWidget):
         for an in self.annot:
             bp = cv2.boxPoints(an.rect)
             bp = np.int0(bp)
-            cv2.drawContours(vis, [bp], 0, (0, 255, 0),3)
-            cv2.putText(vis, '{}'.format(id), (bp[0, 0]+5, bp[0, 1]-5), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0))
+            cv2.drawContours(vis, [bp], 0, (0, 0, 255),3)
+            cv2.putText(vis, '{}'.format(self.classList[int(an.classIndex)]), (bp[0, 0]+5, bp[0, 1]-5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255))
             id += 1
-
-        for idx in np.arange(self.annot_idx):
-            cv2.line(vis, tuple(self.annot_coords[idx]), tuple(self.annot_coords[idx]), (0, 0, 255), 8)
 
         self.label_image.setPixmap(QtGui.QPixmap.fromImage(mat_to_qimage(vis)))
         self.label_image.setGeometry(QtCore.QRect(0, 0, vis.shape[1], vis.shape[0]))
+        self.label_image.updatePixmap(QtGui.QPixmap.fromImage(mat_to_qimage(vis)))
 
     def fetchNextImage(self):
         if self.image_path is not None:
@@ -291,10 +309,10 @@ class Window(QtGui.QWidget):
                 annot_data = []
                 if os.path.isfile(annot_name):
                     print('Loading existing annotation file')
-                    annot_data = parse_annotation_file(annot_name)
+                    annot_data = parse_annotation_file(annot_name,img.shape[0],img.shape[1])
                 self.newImage(filesImage[indexOfImage+1], img, annot_name, annot_data)
                 self.redraw()
-                print("path "+str(indexOfImage))
+
     def fetchPreviousImage(self):
         if self.image_path is not None:
             path, _ = os.path.split(self.image_path)
@@ -306,53 +324,56 @@ class Window(QtGui.QWidget):
                 annot_data = []
                 if os.path.isfile(annot_name):
                     print('Loading existing annotation file')
-                    annot_data = parse_annotation_file(annot_name)
+                    annot_data = parse_annotation_file(annot_name,img.shape[0],img.shape[1])
                 self.newImage(filesImage[indexOfImage-1], img, annot_name, annot_data)
                 self.redraw()
-                print("path "+str(indexOfImage))
+
     def saveAnnotation(self):
         print('Writing annotation data to "{}"'.format(self.annot_path))
-        write_annotation_file(self.annot, self.annot_path)
+        write_annotation_file(self.annot, self.annot_path,self.image.shape[0],self.image.shape[1])
+
+    def selectionchange(self,i):
+        self.classIndx = i
 
     def rotate3Image(self):
         rotated1 = cv2.rotate(self.image, cv2.ROTATE_90_CLOCKWISE)
-        write_annotation_file_90(self.annot, (os.path.splitext(self.annot_path)[0]+'r90.txt'),self.image.shape[0])
+        write_annotation_file_90(self.annot, (os.path.splitext(self.annot_path)[0]+'r90.txt'),self.image.shape[0],self.image.shape[1])
         cv2.imwrite((os.path.splitext(self.annot_path)[0]+'r90.jpg'),rotated1)
         rotated2 = cv2.rotate(self.image, cv2.ROTATE_180)
         write_annotation_file_180(self.annot, (os.path.splitext(self.annot_path)[0]+'r180.txt'),self.image.shape[0],self.image.shape[1])
         cv2.imwrite((os.path.splitext(self.annot_path)[0]+'r180.jpg'),rotated2)
         rotated3 = cv2.rotate(self.image, cv2.ROTATE_90_COUNTERCLOCKWISE)
-        write_annotation_file_270(self.annot, (os.path.splitext(self.annot_path)[0]+'r270.txt'),self.image.shape[1])
+        write_annotation_file_270(self.annot, (os.path.splitext(self.annot_path)[0]+'r270.txt'),self.image.shape[0],self.image.shape[1])
         cv2.imwrite((os.path.splitext(self.annot_path)[0]+'r270.jpg'),rotated3)
 
     def addnoiseImage(self):
         img = self.image.copy()
         noiseImage = cv2.randn(img,(0,0,0),(5,5,5))
-        write_annotation_file(self.annot, (os.path.splitext(self.annot_path)[0]+'n1.txt'))
+        write_annotation_file(self.annot, (os.path.splitext(self.annot_path)[0]+'n1.txt'),self.image.shape[0],self.image.shape[1])
         cv2.imwrite((os.path.splitext(self.annot_path)[0]+'n1.jpg'),(self.image+noiseImage))
         img2 = self.image.copy()
         noiseImage2 = cv2.randn(img2,(0,0,0),(10,10,10))
-        write_annotation_file(self.annot, (os.path.splitext(self.annot_path)[0]+'n2.txt'))
+        write_annotation_file(self.annot, (os.path.splitext(self.annot_path)[0]+'n2.txt'),self.image.shape[0],self.image.shape[1])
         cv2.imwrite((os.path.splitext(self.annot_path)[0]+'n2.jpg'),(self.image+noiseImage2))
 
     def addBrightImage(self):
         frameBright1 = increase_brightness(self.image, value=10)
-        write_annotation_file(self.annot, (os.path.splitext(self.annot_path)[0]+'b1.txt'))
+        write_annotation_file(self.annot, (os.path.splitext(self.annot_path)[0]+'b1.txt'),self.image.shape[0],self.image.shape[1])
         cv2.imwrite((os.path.splitext(self.annot_path)[0]+'b1.jpg'),(frameBright1))
         frameBright2 = increase_brightness(self.image, value=20)
-        write_annotation_file(self.annot, (os.path.splitext(self.annot_path)[0]+'b2.txt'))
+        write_annotation_file(self.annot, (os.path.splitext(self.annot_path)[0]+'b2.txt'),self.image.shape[0],self.image.shape[1])
         cv2.imwrite((os.path.splitext(self.annot_path)[0]+'b2.jpg'),(frameBright2))
 
     def flippingImage(self):
-        horizontal_img = cv2.flip( self.image, 1 )
-        write_annotation_file_flip(self.annot, (os.path.splitext(self.annot_path)[0]+'f.txt'),self.image.shape[1])
+        horizontal_img = cv2.flip( self.image, 1)
+        write_annotation_file_flip(self.annot, (os.path.splitext(self.annot_path)[0]+'f.txt'),self.image.shape[0],self.image.shape[1])
         cv2.imwrite((os.path.splitext(self.annot_path)[0]+'f.jpg'),(horizontal_img))
 
     def closeEvent(self, event):
         reply = QtGui.QMessageBox.question(self, 'Save?', 'Save changes to file?', QtGui.QMessageBox.Yes | QtGui.QMessageBox.No, QtGui.QMessageBox.No)
         if reply == QtGui.QMessageBox.Yes:
             print('Writing annotation data to "{}"'.format(self.annot_path))
-            write_annotation_file(self.annot, self.annot_path)
+            write_annotation_file(self.annot, self.annot_path, self.image.shape[0],self.image.shape[1])
         event.accept()
 
 
